15e84dc1a1a73edd66de98fc775ba20a2860ce75
minshurui <95971968+minshurui@users.noreply.github.com>
2023-08-03 13:37:06 +0800

    Update zI6rtTZC_install.sh

--- zI6rtTZC_install.sh
+++ zI6rtTZC_install.sh
@@ -1,21 +1,3 @@
-#!/bin/bash
-
-# 安装 whiptail
-apt install -y whiptail 
-
-# 欢迎界面
-whiptail --title "安装程序" --msgbox "欢迎使用Alist和Aring安装程序" 10 40
-
-
-
-# 确认安装
-whiptail --title "确认安装" --yesno "是否开始安装?" 10 40
-if [ $? != 0 ]; then
-  exit 1
-fi
-
-
-
 ```bash
 #!/bin/bash
 
@@ -31,7 +13,7 @@ mkdir -p $shared_dir/alist_config
 mkdir -p $shared_dir/aring_data
 
 # 拉取Alist镜像
-docker pull alexwall/alist
+docker pull xhofe/alist:latest
 
 # 运行Alist容器
 docker run -d --name alist \
@@ -39,11 +21,10 @@ docker run -d --name alist \
   -p 5212:5212 \
   -v $shared_dir/alist_data:/alist/data \
   -v $shared_dir/alist_config:/alist/config \
-  alexwall/alist
+  xhofe/alist:latest
 
 # 拉取Aring镜像
-docker pull eilinge/containers are running successfully!"
-```aring2:latest
+docker pull eilinge/aring2:latest
 
 # 运行Aring容器
 docker run -d --name aring \
@@ -52,4 +33,5 @@ docker run -d --name aring \
   -v $shared_dir/aring_data:/data \
   eilinge/aring2:latest
 
-echo "Alist and Aring
+echo "Alist and Aring containers are running successfully!"
+```
